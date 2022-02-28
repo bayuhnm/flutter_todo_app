3647f7fb5ac0014049c331af3e9be2b0100acb86
bayuhnm <bayuhidayahnurm@gmail.com>
2022-02-28 10:46:29 +0700

    Add SafeArea from statusbar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -18,77 +18,79 @@ class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blueGrey,
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 20),
-        child: Column(
-          children: [
-            SizedBox(
-              height: 50,
-            ),
-            Text(
-              'Todo App',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 24,
-                fontWeight: FontWeight.w600,
+      backgroundColor: Colors.blueGrey[900],
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 10,
               ),
-            ),
-            Container(
-              padding: EdgeInsets.only(
-                bottom: 20,
-              ),
-            ),
-            Expanded(
-              child: Column(
-                  children: todos
-                      .map(
-                        (item) => TodoItem(
-                          title: item.title,
-                        ),
-                      )
-                      .toList()),
-            ),
-            Container(
-              height: 60,
-              width: MediaQuery.of(context).size.width,
-              margin: EdgeInsets.only(
-                bottom: 50,
-              ),
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(12),
-              ),
-              child: Center(
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: TextFormField(
-                        controller: todoController,
-                        style: TextStyle(fontSize: 18),
-                        decoration:
-                            InputDecoration.collapsed(hintText: 'Add task...'),
-                      ),
-                    ),
-                    GestureDetector(
-                        onTap: () {
-                          print(todoController.text);
-                          setState(() {
-                            todos.add(
-                              TodoModel(
-                                  title: todoController.text, isDone: false),
-                            );
-
-                            todoController.text = '';
-                          });
-                        },
-                        child: Icon(Icons.add)),
-                  ],
+              Text(
+                'Todo App',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                  fontWeight: FontWeight.w600,
                 ),
               ),
-            ),
-          ],
+              Container(
+                padding: EdgeInsets.only(
+                  bottom: 20,
+                ),
+              ),
+              Expanded(
+                child: Column(
+                    children: todos
+                        .map(
+                          (item) => TodoItem(
+                            title: item.title,
+                          ),
+                        )
+                        .toList()),
+              ),
+              Container(
+                height: 60,
+                width: MediaQuery.of(context).size.width,
+                margin: EdgeInsets.only(
+                  bottom: 50,
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Center(
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: TextFormField(
+                          controller: todoController,
+                          style: TextStyle(fontSize: 18),
+                          decoration:
+                              InputDecoration.collapsed(hintText: 'Add task...'),
+                        ),
+                      ),
+                      GestureDetector(
+                          onTap: () {
+                            print(todoController.text);
+                            setState(() {
+                              todos.add(
+                                TodoModel(
+                                    title: todoController.text, isDone: false),
+                              );
+      
+                              todoController.text = '';
+                            });
+                          },
+                          child: Icon(Icons.add)),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
